fb16af5256aa41cf4d648186d33c1e9facb97309
gordonhu608 <gordonhu608@gmail.com>
2023-05-09 01:32:09 -0700

    projection train

--- llava/train/peft_train_llama_modeling.cpython-310.py
+++
@@ -1,7 +0,0 @@
-# uncompyle6 version 3.9.0
-# Python bytecode version base 3.10.0 (3439)
-# Decompiled from: Python 3.9.16 (main, Mar  8 2023, 14:00:05) 
-# [GCC 11.2.0]
-# Embedded file name: /home/shawn/cube/vl_research/new_LLaVA/llava/train/peft_train_llama_modeling.py
-# Compiled at: 2023-05-01 15:28:48
-# Size of source mod 2**32: 21525 bytes
